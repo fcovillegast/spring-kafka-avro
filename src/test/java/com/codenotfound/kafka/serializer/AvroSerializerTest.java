2305d3565c1ec7f879c50114e44dfca7115c5444
Francisco Villegas <francisco.villegas@peoplemanager.cl>
2019-09-30 15:22:26 -0300

    Initial commit

---
+++ src/test/java/com/codenotfound/kafka/serializer/AvroSerializerTest.java
@@ -0,0 +1,22 @@
+package com.codenotfound.kafka.serializer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.xml.bind.DatatypeConverter;
+
+import org.junit.Test;
+
+public class AvroSerializerTest {
+
+  @Test
+  public void testSerialize() {
+    User user = User.newBuilder().setName("John Doe").setFavoriteColor("green")
+        .setFavoriteNumber(null).build();
+
+    AvroSerializer<User> avroSerializer = new AvroSerializer<>();
+    assertThat(avroSerializer.serialize("avro.t", user))
+        .isEqualTo(DatatypeConverter.parseHexBinary("104A6F686E20446F6502000A677265656E"));
+
+    avroSerializer.close();
+  }
+}
